aa436f0bfc3a969fd1e63134410150011e2c05f9
zetvyy <zetvyy@gmail.com>
2021-12-08 22:15:23 +0700

    feat: fixed styling navbar and page checkout

--- src/Components/checkout-cart/checkout-cart.js
+++ src/Components/checkout-cart/checkout-cart.js
@@ -7,9 +7,9 @@ import { FaTrash } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
 import { FaClipboardList } from "react-icons/fa";
 import { AiOutlineMinus } from "react-icons/ai";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
-function Checkoutcart() {
+function CheckoutCart() {
   const [visible, setVisible] = useState(false);
   const toggleMenu = () => {
     setVisible(!visible);
@@ -24,16 +24,16 @@ function Checkoutcart() {
             <img src={foto} alt="Cravyng Logo" />
             {/* <a href="#">Continue as merchant</a> */}
           </div>
-          
-          <div className={Styles.Cart} onClick={() => history.push('/profile')} >
-            
-              <RiAccountCircleFill  />
-              Heavenly Taste |
-            
-          </div>
-          
-          <div className={Styles.Cart2} onClick={() => toggleMenu()}>
-            <MdShoppingBasket /> 1 Items
+
+          <div className={Styles.cart_container}>
+            <div className={Styles.Cart} onClick={() => history.push("/profile")}>
+              <RiAccountCircleFill className={Styles.icon_account} />
+              Heavenly Taste
+            </div>
+
+            <div className={Styles.Cart2} onClick={() => toggleMenu()}>
+              <MdShoppingBasket className={Styles.icon_cart} /> 1 Items
+            </div>
           </div>
         </nav>
         {visible && (
@@ -103,7 +103,7 @@ function Checkoutcart() {
               <p>Total payment </p>
               <p>Rp 352.000</p>
             </div>
-            <div className={Styles.buttonCheck} onClick={() => history.push('/checkout')} >
+            <div className={Styles.buttonCheck} onClick={() => history.push("/checkout")}>
               <button> Go to Checkout </button>
             </div>
           </div>
@@ -113,4 +113,4 @@ function Checkoutcart() {
   );
 }
 
-export default Checkoutcart;
+export default CheckoutCart;
